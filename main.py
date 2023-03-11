7181fe3beffdc346a566038641fbb412cb2e4bc8
Bellotte <127588022+Bellotte@users.noreply.github.com>
2023-03-11 11:23:21 +0100

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-def on_button_pressed_a():
-    global Target, Menu
-    if Menu == "START":
-        Target = Target + 5
-    else:
-        Menu = "START"
-        Target = input.compass_heading()
-        basic.show_icon(IconNames.HOUSE)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_logo_touched():
+    global Target
+    #Target = input.compass_heading()
+    music.play_tone(444, music.beat(BeatFraction.QUARTER))
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
 
-def on_button_pressed_b():
+def on_button_pressed_a():
     global Target, Menu
     if Menu == "START":
         Target = Target - 5
     else:
         Menu = "START"
-        Target = input.compass_heading()
         basic.show_icon(IconNames.HOUSE)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_logo_long_pressed():
+    music.play_tone(222, music.beat(BeatFraction.QUARTER))
+    input.calibrate_compass()
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
 
 def on_button_pressed_ab():
     global Menu
     Menu = "STOP"
+    music.play_tone(444, music.beat(BeatFraction.HALF))
     basic.show_icon(IconNames.SKULL)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global Target, Menu
+    if Menu == "START":
+        Target = Target + 5
+    else:
+        Menu = "START"
+        basic.show_icon(IconNames.HOUSE)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def CalcDelta(fTarget: number):
     global fDelta
@@ -34,37 +44,35 @@ def CalcDelta(fTarget: number):
 MoyDelta = 0
 index = 0
 SommeDelta = 0
+Delta = 0
 Target = 0
 fDelta = 0
 Menu = ""
 Menu = "STOP"
-NbIndex = 6
-fDelta = 999
-Target = 999
-Delta = 999
+NbIndex = 4
+fDelta = 0
 servos.P0.set_angle(90)
-EchantDelta = [0, 0, 0, 0, 0, 0]
+EchantDelta = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 basic.show_icon(IconNames.SKULL)
 
 def on_every_interval():
-    global Delta, Menu
-    if Menu == "START":
-        # basic.show_number(Target)
-        Delta = CalcDelta(Target)
-        EchantDelta.unshift(Delta)
-loops.every_interval(200, on_every_interval)
+    global Target, Delta
+    if Menu == "STOP":
+        Target = input.compass_heading()
+    # basic.show_number(Target)
+    Delta = CalcDelta(Target)
+    EchantDelta.unshift(Delta)
+loops.every_interval(500, on_every_interval)
 
 def on_every_interval2():
-    global SommeDelta, index, MoyDelta, Menu
+    global SommeDelta, index, MoyDelta
     if Menu == "START":
-        # Delta = CalcDelta(Target)
-        basic.show_number(Delta)
         SommeDelta = 0
         index = 0
         while index <= NbIndex - 1:
             SommeDelta += EchantDelta[index]
             index += 1
         MoyDelta = Math.round(SommeDelta / NbIndex)
-        basic.show_string("" + str(MoyDelta))
+        basic.show_string("" + str((MoyDelta)))
         servos.P0.set_angle(90 + MoyDelta)
-loops.every_interval(5000, on_every_interval2)
+loops.every_interval(3000, on_every_interval2)
--- main.ts
+++ main.ts
@@ -1,29 +1,37 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_touched() {
     
-    if (Menu == "START") {
-        Target = Target + 5
-    } else {
-        Menu = "START"
-        Target = input.compassHeading()
-        basic.showIcon(IconNames.House)
-    }
-    
+    // Target = input.compass_heading()
+    music.playTone(444, music.beat(BeatFraction.Quarter))
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     if (Menu == "START") {
         Target = Target - 5
     } else {
         Menu = "START"
-        Target = input.compassHeading()
         basic.showIcon(IconNames.House)
     }
     
 })
+input.onLogoEvent(TouchButtonEvent.LongPressed, function on_logo_long_pressed() {
+    music.playTone(222, music.beat(BeatFraction.Quarter))
+    input.calibrateCompass()
+})
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
     Menu = "STOP"
+    music.playTone(444, music.beat(BeatFraction.Half))
     basic.showIcon(IconNames.Skull)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (Menu == "START") {
+        Target = Target + 5
+    } else {
+        Menu = "START"
+        basic.showIcon(IconNames.House)
+    }
+    
 })
 function CalcDelta(fTarget: number): number {
     
@@ -39,31 +47,29 @@ function CalcDelta(fTarget: number): number {
 let MoyDelta = 0
 let index = 0
 let SommeDelta = 0
+let Delta = 0
 let Target = 0
 let fDelta = 0
 let Menu = ""
 Menu = "STOP"
-let NbIndex = 6
-fDelta = 999
-Target = 999
-let Delta = 999
+let NbIndex = 4
+fDelta = 0
 servos.P0.setAngle(90)
-let EchantDelta = [0, 0, 0, 0, 0, 0]
+let EchantDelta = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 basic.showIcon(IconNames.Skull)
-loops.everyInterval(200, function on_every_interval() {
+loops.everyInterval(500, function on_every_interval() {
     
-    if (Menu == "START") {
-        //  basic.show_number(Target)
-        Delta = CalcDelta(Target)
-        EchantDelta.unshift(Delta)
+    if (Menu == "STOP") {
+        Target = input.compassHeading()
     }
     
+    //  basic.show_number(Target)
+    Delta = CalcDelta(Target)
+    EchantDelta.unshift(Delta)
 })
-loops.everyInterval(5000, function on_every_interval2() {
+loops.everyInterval(3000, function on_every_interval2() {
     
     if (Menu == "START") {
-        //  Delta = CalcDelta(Target)
-        basic.showNumber(Delta)
         SommeDelta = 0
         index = 0
         while (index <= NbIndex - 1) {
