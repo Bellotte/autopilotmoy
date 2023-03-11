5cadfed289822559ccd6438244cfcb158f98c645
Bellotte <127588022+Bellotte@users.noreply.github.com>
2023-03-11 08:41:08 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,70 @@
+def on_button_pressed_a():
+    global Target, Menu
+    if Menu == "START":
+        Target = Target + 5
+    else:
+        Menu = "START"
+        Target = input.compass_heading()
+        basic.show_icon(IconNames.HOUSE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    Menu = "STOP"
+    basic.show_icon(IconNames.SKULL)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global Target, Menu
+    if Menu == "START":
+        Target = Target - 5
+    else:
+        Menu = "START"
+        Target = input.compass_heading()
+        basic.show_icon(IconNames.HOUSE)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def CalcDelta(fTarget: number):
+    global fDelta
+    if input.compass_heading() - fTarget > 180:
+        fDelta = input.compass_heading() - 360 - fTarget
+    else:
+        fDelta = input.compass_heading() - fTarget
+    return fDelta
+MoyDelta = 0
+index = 0
+SommeDelta = 0
+Target = 0
+fDelta = 0
+Menu = ""
+Menu = "STOP"
+NbIndex = 6
+fDelta = 999
+Target = 999
+Delta = 999
+servos.P0.set_angle(90)
+EchantDelta = [0, 0, 0, 0, 0, 0]
+TabOK = 1
+basic.show_icon(IconNames.SKULL)
+
+def on_every_interval():
+    global Delta
+    if Menu == "START":
+        # basic.show_number(Target)
+        Delta = CalcDelta(Target)
+        EchantDelta.unshift(Delta)
+loops.every_interval(200, on_every_interval)
+
+def on_every_interval2():
+    global SommeDelta, index, MoyDelta
+    if Menu == "START":
+        # Delta = CalcDelta(Target)
+        basic.show_number(Delta)
+        SommeDelta = 0
+        index = 0
+        while index <= NbIndex - 1:
+            SommeDelta += EchantDelta[index]
+            index += 1
+        MoyDelta = Math.round(SommeDelta / NbIndex)
+        basic.show_string("" + str(MoyDelta))
+        servos.P0.set_angle(90 + MoyDelta)
+loops.every_interval(5000, on_every_interval2)
---
+++ main.ts
@@ -0,0 +1,78 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (Menu == "START") {
+        Target = Target + 5
+    } else {
+        Menu = "START"
+        Target = input.compassHeading()
+        basic.showIcon(IconNames.House)
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let Menu = "STOP"
+    basic.showIcon(IconNames.Skull)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (Menu == "START") {
+        Target = Target - 5
+    } else {
+        Menu = "START"
+        Target = input.compassHeading()
+        basic.showIcon(IconNames.House)
+    }
+    
+})
+function CalcDelta(fTarget: number): number {
+    
+    if (input.compassHeading() - fTarget > 180) {
+        fDelta = input.compassHeading() - 360 - fTarget
+    } else {
+        fDelta = input.compassHeading() - fTarget
+    }
+    
+    return fDelta
+}
+
+let MoyDelta = 0
+let index = 0
+let SommeDelta = 0
+let Target = 0
+let fDelta = 0
+let Menu = ""
+Menu = "STOP"
+let NbIndex = 6
+fDelta = 999
+Target = 999
+let Delta = 999
+servos.P0.setAngle(90)
+let EchantDelta = [0, 0, 0, 0, 0, 0]
+let TabOK = 1
+basic.showIcon(IconNames.Skull)
+loops.everyInterval(200, function on_every_interval() {
+    
+    if (Menu == "START") {
+        //  basic.show_number(Target)
+        Delta = CalcDelta(Target)
+        EchantDelta.unshift(Delta)
+    }
+    
+})
+loops.everyInterval(5000, function on_every_interval2() {
+    
+    if (Menu == "START") {
+        //  Delta = CalcDelta(Target)
+        basic.showNumber(Delta)
+        SommeDelta = 0
+        index = 0
+        while (index <= NbIndex - 1) {
+            SommeDelta += EchantDelta[index]
+            index += 1
+        }
+        MoyDelta = Math.round(SommeDelta / NbIndex)
+        basic.showString("" + ("" + MoyDelta))
+        servos.P0.setAngle(90 + MoyDelta)
+    }
+    
+})
